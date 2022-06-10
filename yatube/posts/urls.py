075201ea9b45291a44a5943dd2d9a6a1357305e8
Ivan Reznichenko <ivreznic@gmail.com>
2022-06-10 10:01:23 +0300

    remove superfluous commemts

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -6,15 +6,10 @@ app_name = "posts"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    # Posts in certain community
     path("group/<slug:slug>/", views.group_posts, name="group_list"),
-    # User profile and his posts
     path("profile/<str:username>/", views.profile, name="profile"),
-    # Single post view
     path("posts/<int:post_id>/", views.post_detail, name="post_detail"),
-    # Edit post
     path("posts/<int:post_id>/edit/", views.post_edit, name="post_edit"),
-    # Add comment
     path(
         "posts/<int:post_id>/comment/", views.add_comment, name="add_comment"
     ),
