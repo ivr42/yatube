3249f3d5d658d06b649db33dc23b71461bfdcf18
Ivan Reznichenko <ivreznic@gmail.com>
2022-06-10 10:02:24 +0300

    register Follow and Comment in admin.py

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 
-from .models import Group, Post
+from .models import Comment, Follow, Group, Post
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("pk", "text", "created", "author", "post")
+    list_editable = (
+        "author",
+        "post",
+    )
+    search_fields = ("text",)
+    list_filter = ("created", "author")
+    empty_value_display = "-пусто-"
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ("pk", "author", "user")
+    list_editable = (
+        "author",
+        "user",
+    )
+    list_filter = ("user", "author")
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -17,5 +37,7 @@ class GroupAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
 
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Follow, FollowAdmin)
 admin.site.register(Group, GroupAdmin)
 admin.site.register(Post, PostAdmin)
