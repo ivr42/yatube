cf3b88dc05fb2c0c990756b7c371e4b64d384d7d
Ivan Reznichenko <ivreznic@gmail.com>
2022-07-15 15:16:44 +0300

    API converted to APIView class

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -239,3 +239,73 @@ def api_posts(request: Request) -> Response:
     posts = Post.objects.all()
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class APIPost(APIView):
+    def get(self, request: Request) -> Response:
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+    def post(self, request: Request) -> Response:
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class APIPostDetail(APIView):
+    def get(self, request: Request, pk: int) -> Response:
+        try:
+            post = Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            return Response(
+                {"detail": f"Post id={pk} does not exists"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    def put(self, request: Request, pk: int) -> Response:
+        try:
+            post = Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            return Response(
+                {"detail": f"Post id={pk} does not exists"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        serializer = PostSerializer(post, data=request.data, partial=False)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request: Request, pk: int) -> Response:
+        try:
+            post = Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            return Response(
+                {"detail": f"Post id={pk} does not exists"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        serializer = PostSerializer(post, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request: Request, pk: int) -> Response:
+        try:
+            post = Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            return Response(
+                {"detail": f"Post id={pk} does not exists"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
