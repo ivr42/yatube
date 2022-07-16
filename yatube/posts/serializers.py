f2b1a39dc343e999328210e5c6618de723e581cb
Ivan Reznichenko <ivreznic@gmail.com>
2022-07-16 15:45:11 +0300

    API: display/modify group slug for every post

--- yatube/posts/serializers.py
+++ yatube/posts/serializers.py
@@ -1,9 +1,22 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
 
-from .models import Post
+from .models import Group, Post
+
+
+class GroupSerializer(ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ("id", "title", "slug", "description")
 
 
 class PostSerializer(ModelSerializer):
+    group = SlugRelatedField(
+        required=False,
+        allow_null=True,
+        slug_field="slug",
+        queryset=Group.objects.all(),
+    )
+
     class Meta:
         model = Post
-        fields = ("id", "text", "author", "image", "created")
+        fields = ("id", "text", "author", "image", "created", "group")
